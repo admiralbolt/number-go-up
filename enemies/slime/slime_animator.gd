b9d18d9d9bb853b072357ec1af3273c16127171b
admiralbolt <aviknecht@gmail.com>
2026-03-17 20:34:06 -0700

    Slightly speedup attack animation.

--- enemies/slime/slime_animator.gd
+++ enemies/slime/slime_animator.gd
@@ -17,7 +17,7 @@ func generate_animations() -> void:
   EnemyAnimator.add_animation_entries(animation_entries, "idle", "res://assets/monsters/slime1/Slime1_Idle_with_shadow.png", 6, direction_order, 4, 1.0, Animation.LoopMode.LOOP_LINEAR)
   EnemyAnimator.add_animation_entries(animation_entries, "walk", "res://assets/monsters/slime1/Slime1_Walk_with_shadow.png", 8, direction_order, 4, 1.2, Animation.LoopMode.LOOP_LINEAR)
   EnemyAnimator.add_animation_entries(animation_entries, "run", "res://assets/monsters/slime1/Slime1_Run_with_shadow.png", 8, direction_order, 4, 1.0, Animation.LoopMode.LOOP_LINEAR)
-  EnemyAnimator.add_animation_entries(animation_entries, "attack", "res://assets/monsters/slime1/slime1_attack_full.png", 10, direction_order, 4, 1.8, Animation.LoopMode.LOOP_NONE)
+  EnemyAnimator.add_animation_entries(animation_entries, "attack", "res://assets/monsters/slime1/slime1_attack_full.png", 10, direction_order, 4, 1.4, Animation.LoopMode.LOOP_NONE)
 
   EnemyAnimator.create_animations(animator, self.get_path_to(sprite), animation_entries)
 
