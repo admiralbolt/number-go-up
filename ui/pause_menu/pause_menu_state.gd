153f0d88ad3e0fdcd7494b231f866eec51aa0fd8
admiralbolt <aviknecht@gmail.com>
2026-05-12 17:16:19 -0700

    Gah

--- ui/pause_menu/pause_menu_state.gd
+++ ui/pause_menu/pause_menu_state.gd
@@ -14,21 +14,6 @@ var secondary_tab_focus_name: Dictionary[String, String] = {
 
 }
 
-
-# If the primary tab bar is focused.
-var primary_tab_bar_focused: bool = true
-
-# Which button is focused in the character menu.
-var character_menu_focus_index: int = 0
-
-
-# Which button is focused in the button menu.
-var button_sidebar_focus_index: int = 0
-
-# Which button is focused in the IO menu.
-var io_menu_focus_index: int = 0
-
-
 # Which item is selected in the attributes sub menu.
 var attribute_focus_index: int = 0
 
@@ -43,7 +28,6 @@ var overview_focus_index: int = 0
 
 # Focused item index in sub menu for player inventory.
 var player_inventory_sub_type_focus_index: Dictionary[Item.ItemType, int] = {}
-
 var player_equipment_sub_type_focus_index: Dictionary[Equipment.EquipmentSlot, int] = {}
 
 func debug_print() -> void:
